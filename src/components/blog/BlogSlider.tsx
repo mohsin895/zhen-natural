e6f253870e80fe7144e095823669d6306f5eb7c9
Essential-Infotech <33011566+mohsin895@users.noreply.github.com>
2026-04-20 00:08:38 +0600

    blog update

--- src/components/blog/BlogSlider.tsx
+++ src/components/blog/BlogSlider.tsx
@@ -40,7 +40,7 @@ const BlogSlider: React.FC<BlogSliderProps> = ({ hasPaginate = false }) => {
   }, []);
 
   // if (error) return <div>Failed to load blogs</div>;
-  // if (!data) return <div>Loading blogs...</div>;
+   if (!data) return <div></div>;
 
   const blogs: Blog[] = data.blogs.data;
   const formatDate = (dateStr: string) => {
